89db2609359fbe14f35fa1562494c012688cc6fa
Julien Jelev <78555225+Jelev123@users.noreply.github.com>
2022-06-08 12:05:56 +0300

    .

--- Data/TheGoodOldLibrary.Data.Models/Book.cs
+++ Data/TheGoodOldLibrary.Data.Models/Book.cs
@@ -1,6 +1,5 @@
 ﻿namespace TheGoodOldLibrary.Data.Models
 {
-    using System;
     using System.Collections.Generic;
 
     using TheGoodOldLibrary.Data.Common.Models;
@@ -26,6 +25,5 @@
         public virtual ICollection<ApplicationUser> Users { get; set; } = new HashSet<ApplicationUser>();
 
         public virtual ICollection<BookTaking> BookTakings { get; set; } = new HashSet<BookTaking>();
-
     }
 }
